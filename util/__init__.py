434cc4a14d2ed35b5327dabc7ee0ed4583886962
Clam <clam@nyanya.org>
2011-09-26 20:21:01 +1000

    Added priority for Mapping.
     - Also added a quick, cheap and easy way to do SUPER OVERRIDE with priority=0
     - Might be a bit weird on regex, see comments in dispatcher.py

--- util/__init__.py
+++ util/__init__.py
@@ -12,7 +12,7 @@ def dummyfunc(event, botinstance, dbqueue):
 	pass
 
 class Mapping:
-	def __init__(self, types=[], command=None, regex=None, function = dummyfunc):
+	def __init__(self, types=[], command=None, regex=None, function = dummyfunc, priority=10):
 		# type = [list of strings], command=string, regex=compiledRegExobject, 
 		#		function=a defined function should be expecting the following arguments:
 		# def dummyfunc(event, botinstance, dbqueue):
@@ -20,3 +20,4 @@ class Mapping:
 		self.command = command
 		self.regex = regex
 		self.function = function
+		self.priority = priority
--- util/dispatcher.py
+++ util/dispatcher.py
@@ -4,16 +4,11 @@ from settings import Settings
 from sys import stderr
 from traceback import format_exc
 from os.path import join
+from operator import attrgetter
 
 from state import State
 from wrapper import BotWrapper
-# class DispatchType:
-	
-	# def __init__(self):
-		# self.instant = []
-		# self.commands = []
-		# self.regex = []
-
+		
 class Dispatcher:
 	modules = []
 	hostmap = {}
@@ -25,12 +20,30 @@ class Dispatcher:
 		if type == "sendmsg":
 			cls.hostmap[sever]["MSGHOOKS"] = True
 		if (not mapping.command) and (not mapping.regex): 
-			cls.hostmap[sever][type]["instant"].append(mapping.function)
+			cls.hostmap[sever][type]["instant"].append(mapping)
+			cls.hostmap[sever][type]["instant"].sort(key=attrgetter('priority'))
 		else:
 			if mapping.command:
-				cls.hostmap[sever][type]["command"].append((mapping.command, mapping.function))
+				#check if tuple or list or basestring (str or unicodes)
+				# I guess it's not a big deal to check both
+				mapcom = mapping.command
+				if isinstance(mapcom, list) or isinstance(mapcom, tuple):
+					for com in mapcom:
+						if com in cls.hostmap[sever][type]["command"]:
+							cls.hostmap[sever][type]["command"][com].append(mapping)
+						else:
+							cls.hostmap[sever][type]["command"][com] = [mapping]
+						cls.hostmap[sever][type]["command"][com].sort(key=attrgetter('priority'))	
+				elif isinstance(mapcom, basestring):
+					if mapcom in cls.hostmap[sever][type]["command"]:
+						cls.hostmap[sever][type]["command"][mapcom].append(mapping)
+					else:
+						cls.hostmap[sever][type]["command"][mapcom] = [mapping]
+				cls.hostmap[sever][type]["command"][mapcom].sort(key=attrgetter('priority'))
+			
 			if mapping.regex:
-				cls.hostmap[sever][type]["regex"].append((mapping.regex, mapping.function))
+				cls.hostmap[sever][type]["regex"].append(mapping)
+				cls.hostmap[sever][type]["regex"].sort(key=attrgetter('priority'))
 	
 	@classmethod
 	def reload(cls, callback=None):
@@ -48,7 +61,7 @@ class Dispatcher:
 				cls.hostmap[servername][type] = {}
 				cls.hostmap[servername]["MSGHOOKS"] = False
 				cls.hostmap[servername][type]["instant"] = []
-				cls.hostmap[servername][type]["command"] = []
+				cls.hostmap[servername][type]["command"] = {}
 				cls.hostmap[servername][type]["regex"] = []
 			if servers[servername].allowmodules:
 				modulemap[servername] = servers[servername].allowmodules
@@ -99,7 +112,12 @@ class Dispatcher:
 				print >> stderr, traceback
 		else: print "All done."
 		
-	
+	#TODO: Put a Queue somewhere so that events that happen while a RELOAD is in progress can be queued and then processed
+	#		after the reload
+	#	Either that, or maybe we should construct the new hostmap, then overwrite the original when reload is complete.
+	#		10million easier to do the latter, but the current reload implementation would need to be changed a little to
+	#		pass around the temporary hostmap
+	#	
 	@classmethod
 	def dispatch(cls, servername, event):
 		cont_or_wrap = State.networks[servername].container
@@ -125,14 +143,20 @@ class Dispatcher:
 			command = command.lower()
 		
 		#lol dispatcher is 100 more simple now, but at the cost of more dict...
-		for func in cls.hostmap[servername][type]["instant"]:
-			cls._dispatchreally(func, event, cont_or_wrap)
-		for com, func in cls.hostmap[servername][type]["command"]:
-			if command == com:
-				cls._dispatchreally(func, event, cont_or_wrap)
-		for regex, func in cls.hostmap[servername][type]["regex"]:
-			if regex.match(msg):
-				cls._dispatchreally(func, event, cont_or_wrap)
+		for mapping in cls.hostmap[servername][type]["instant"]:
+			cls._dispatchreally(mapping.function, event, cont_or_wrap)
+			if mapping.priority == 0: break #lol cheap and easy way to support total override
+		#super fast command dispatching now... Only thing left that's slow is the regex but has to be
+		if command in cls.hostmap[servername][type]["command"]:
+			for mapping in cls.hostmap[servername][type]["command"][command]:
+				cls._dispatchreally(mapping.function, event, cont_or_wrap)
+				if mapping.priority == 0: break
+		# TODO: Considder this:
+		# super priority==0 override doesn't really make much sense on a regex, but whatever
+		for mapping in cls.hostmap[servername][type]["regex"]:
+			if mapping.regex.match(msg):
+				cls._dispatchreally(mapping.function, event, cont_or_wrap)
+				if mapping.priority == 0: break
 
 	@staticmethod					
 	def _dispatchreally(func, event, cont_or_wrap):

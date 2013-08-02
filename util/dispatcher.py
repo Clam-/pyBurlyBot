1287deb8eebcf1c98711014f9a4a63bcd42471f8
Griffin <grause@gmail.com>
2013-08-02 05:22:50 -0400

    Implemented hostmap hotswap TODO in dispatcher.py

--- util/dispatcher.py
+++ util/dispatcher.py
@@ -15,37 +15,8 @@ class Dispatcher:
 	hostwaitmap = {}
 	TYPES = ('action', 'connectionMade', 'irc_NICK', 'joined', 'privmsg', 
 		'sendmsg', 'signedOn', 'userRenamed')
-	
-	@classmethod
-	def _addmap(cls, sever, type, mapping):
-		if type == "sendmsg":
-			cls.hostmap[sever]["MSGHOOKS"] = True
-		if (not mapping.command) and (not mapping.regex): 
-			cls.hostmap[sever][type]["instant"].append(mapping)
-			cls.hostmap[sever][type]["instant"].sort(key=attrgetter('priority'))
-		else:
-			if mapping.command:
-				#check if tuple or list or basestring (str or unicodes)
-				# I guess it's not a big deal to check both
-				mapcom = mapping.command
-				if isinstance(mapcom, list) or isinstance(mapcom, tuple):
-					for com in mapcom:
-						if com in cls.hostmap[sever][type]["command"]:
-							cls.hostmap[sever][type]["command"][com].append(mapping)
-						else:
-							cls.hostmap[sever][type]["command"][com] = [mapping]
-						cls.hostmap[sever][type]["command"][com].sort(key=attrgetter('priority'))	
-				elif isinstance(mapcom, basestring):
-					if mapcom in cls.hostmap[sever][type]["command"]:
-						cls.hostmap[sever][type]["command"][mapcom].append(mapping)
-					else:
-						cls.hostmap[sever][type]["command"][mapcom] = [mapping]
-				cls.hostmap[sever][type]["command"][mapcom].sort(key=attrgetter('priority'))
-			
-			if mapping.regex:
-				cls.hostmap[sever][type]["regex"].append(mapping)
-				cls.hostmap[sever][type]["regex"].sort(key=attrgetter('priority'))
-	
+
+	# Construct the new hostmap, then overwrite the original when reload is complete.
 	@classmethod
 	def reload(cls, callback=None):
 		print "LOADING..."
@@ -54,20 +25,20 @@ class Dispatcher:
 		#reload all modules I guess
 		#here we should get modules from settings... Assume settings has been updated first.
 		modulemap = {}
-		cls.hostmap = {}
+		hostmap = {}
 		servers = Settings.servers
 		for servername in servers:
-			cls.hostmap[servername] = {}
+			hostmap[servername] = {}
 			if servername not in cls.hostwaitmap:
 				cls.hostwaitmap[servername] = {}
 				for type in cls.TYPES:
 					cls.hostwaitmap[servername][type] = {}
 			for type in cls.TYPES:
-				cls.hostmap[servername][type] = {}
-				cls.hostmap[servername]["MSGHOOKS"] = False
-				cls.hostmap[servername][type]["instant"] = []
-				cls.hostmap[servername][type]["command"] = {}
-				cls.hostmap[servername][type]["regex"] = []
+				hostmap[servername][type] = {}
+				hostmap[servername]["MSGHOOKS"] = False
+				hostmap[servername][type]["instant"] = []
+				hostmap[servername][type]["command"] = {}
+				hostmap[servername][type]["regex"] = []
 			if servers[servername].allowmodules:
 				modulemap[servername] = servers[servername].allowmodules
 			else: 
@@ -78,51 +49,73 @@ class Dispatcher:
 		
 		notloaded = []
 		from imp import find_module, load_module
-		Settings.moduledict = {}
-		for mod in Settings.modules:
+		moduledict = {}
+		for modulename in Settings.modules:
 			try:
-				(f, pathname, description) = find_module(mod, [moddir])
+				(f, pathname, description) = find_module(modulename, [moddir])
 				try:
-					module = load_module(mod, f, pathname, description)
+					module = load_module(modulename, f, pathname, description)
 				except Exception as e:
-					notloaded.append((mod, format_exc()))
+					notloaded.append((modulename, format_exc()))
+					continue
+				finally:
 					f.close()
+			except Exception as e:
+				notloaded.append((mod, format_exc()))
+				continue
+			# Catch errors that might be thrown on running module.init()
+			try:
+				if not module.init():
+					notloaded.append((modulename, "Error in init()"))
 					continue
 			except Exception as e:
-				notloaded.append((mod, format_exc))
-				continue
-			try:
-				if module.init():
-					Settings.moduledict[mod] = module
-					#do stuff with module.mappings
-					#cls.mappings[mod] = []
-					for mapping in module.mappings:
-						for type in mapping.types:
-							if type not in cls.TYPES:
-								print "WARNING UNSUPPORTED TYPE: %s" % type
-							else:
-								for server in modulemap:
-									if mod in modulemap[server]:
-										#add type to servermapping
-										cls._addmap(server, type, mapping)		
-				else:
-					notloaded.append((mod, "Error in init()"))
-			except Exception as e:
-				notloaded.append((mod, format_exc))
+				notloaded.append((modulename, "Error in init():\n\n" + format_exc()))
+
+			Settings.moduledict[modulename] = module
+			for mapping in module.mappings:
+				for type in mapping.types:
+					if type not in cls.TYPES:
+						print "WARNING UNSUPPORTED TYPE: %s" % type
+						continue
+
+					for server in modulemap:
+						if modulename not in modulemap[server]:
+							continue
+
+						# Add type to servermapping
+						# This is _addmap inlined
+						if type == "sendmsg":
+							hostmap[server]["MSGHOOKS"] = True
+						if not mapping.command and not mapping.regex: 
+							hostmap[server][type]["instant"].append(mapping)
+							hostmap[server][type]["instant"].sort(key=attrgetter('priority'))
+
+						if mapping.command:
+							mapcom = mapping.command
+							#check if tuple or list or basestring (str or unicodes)
+							# I guess it's not a big deal to check both
+							if isinstance(mapcom, list) or isinstance(mapcom, tuple):
+								for commandname in mapcom:
+									hostmap[server][type]["command"].setdefault(commandname, []).append(mapping)
+									hostmap[server][type]["command"][commandname].sort(key=attrgetter('priority'))
+							# TODO: unicode command?
+							elif isinstance(mapcom, basestring):
+								hostmap[server][type]["command"].setdefault(mapcom, []).append(mapping)
+								hostmap[server][type]["command"][mapcom].sort(key=attrgetter('priority'))
+
+						if mapping.regex:
+							hostmap[server][type]["regex"].append(mapping)
+							hostmap[server][type]["regex"].sort(key=attrgetter('priority'))
 
 		if notloaded:
 			print "WARNING: MODULE(S) NOT LOADED: %s" % ', '.join((x[0] for x in notloaded))
 			for module, traceback in notloaded:
 				print >> stderr, module + ':'
 				print >> stderr, traceback
-		else: print "All done."
-		
-	#TODO: Put a Queue somewhere so that events that happen while a RELOAD is in progress can be queued and then processed
-	#		after the reload
-	#	Either that, or maybe we should construct the new hostmap, then overwrite the original when reload is complete.
-	#		10million easier to do the latter, but the current reload implementation would need to be changed a little to
-	#		pass around the temporary hostmap
-	#	
+		else:
+			cls.hostmap = hostmap
+			print "All done."
+
 	@classmethod
 	def dispatch(cls, botinst, event):
 		settings = botinst.settings
@@ -134,7 +127,7 @@ class Dispatcher:
 		type = event.type
 		command = ""
 		input = ""
-		if (type != "sendmsg") and msg and msg.startswith(settings.commandprefix):
+		if type != "sendmsg" and msg and msg.startswith(settings.commandprefix):
 			#case insensitive match?
 			#also this means that commands can't have spaces in them, and lol command prefix can't be a space
 			#all are good to me, if you want a case sensitive match you can do your command as a regex - griff

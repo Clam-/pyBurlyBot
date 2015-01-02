14bb27f68cfce179c57f9bbad20a619969b1f718
Clam <clam@nyanya.org>
2015-01-03 05:46:20 +1100

    Display location names better in weather/forecast for some locations

--- modules/weather.py
+++ modules/weather.py
@@ -15,13 +15,13 @@ LOC_MODULE = None
 # Weather for Lansing, MI: 32.2F (0.1C), Wind Chill of 25F (-4C), Partly Cloudy, Humidity 67%, Wind from the East at 8.0mph (12.9km/h) 
 #gusting to 14.0mph (22.5km/h), Low/High 38F/41F (3C/5C).  Flurries or snow showers possible early. A mix of clouds and sun. 
 #High 41F. Winds S at 10 to 20 mph.
-WEATHER_RPL = "Weather for %s, %s, %s: \x02%sF\x02 (\x02%sC\x02), Low/High \x02%sF\x02/\x02%sF\x02 (\x02%sC\x02/\x02%sC\x02), %s, Humidity %s, %s %s"
-WEATHER_RPL_WC = "Weather for %s, %s, %s: \x02%sF\x02 (\x02%sC\x02), Wind Chill of \x02%sF\x02 (\x02%sC\x02), Low/High \x02%sF\x02/\x02%sF\x02 (\x02%sC\x02/\x02%sC\x02), %s, Humidity %s, %s %s"
+WEATHER_RPL = "Weather for %s: \x02%sF\x02 (\x02%sC\x02), Low/High \x02%sF\x02/\x02%sF\x02 (\x02%sC\x02/\x02%sC\x02), %s, Humidity %s, %s %s"
+WEATHER_RPL_WC = "Weather for %s: \x02%sF\x02 (\x02%sC\x02), Wind Chill of \x02%sF\x02 (\x02%sC\x02), Low/High \x02%sF\x02/\x02%sF\x02 (\x02%sC\x02/\x02%sC\x02), %s, Humidity %s, %s %s"
 GHETTOWIND_REGEX = recompile(r'Winds [NSEW]{1,3} at (\d+) to (\d+) (km/h)\.')
 GHETTOTEMP_REGEX = recompile(r'\. (?:High|Low) (?:near )?(?:-)?\d+C\.')
 
 # Forecast for Ann Arbor, MI: Today - Chance of Showers 54F/77F (12C/25C), Wed - Clear 55F/81F (13C/27C), Thu - Mostly Sunny 57F/84F (14C/29C), Fri - Mostly Sunny 63F/88F (17C/31C)
-FORECAST_RPL = "Forecast for %s, %s, %s: %s"
+FORECAST_RPL = "Forecast for %s: %s"
 # Today - Chance of Showers 54F/77F (12C/25C) PoP: %s Hum: %s
 FORECAST_DAY = "%s - %s \x02%sF\x02/\x02%sF\x02 (\x02%sC\x02/\x02%sC\x02) PoP: \x02%s%%\x02 Hum: \x02%s%%\x02"
 def _formatWind(matchobj):
@@ -39,6 +39,13 @@ def _formatWind(matchobj):
 	
 	return "%s%s%s%s %s" % (pre, speed1, orig[mpos[1][1]:mpos[2][0]], speed2, "MPH/KPH")
 
+def _build_locname(loc):
+	l = []
+	if loc['city']: l.append(loc['city'])
+	if loc['state']: l.append(loc['state'])
+	if loc['country_iso3166']: l.append(loc['country_iso3166'])
+	return ", ".join(l)
+
 def weather(event, bot):
 	""" weather [user/location]. If user/location is not provided, weather is displayed for the requesting nick.
 	Otherwise the weather for the requested user/location is displayed."""
@@ -52,7 +59,6 @@ def weather(event, bot):
 	# replace . High 14F. with nothing.
 	outlook = GHETTOTEMP_REGEX.sub(".", outlook)
 	fore = fore['simpleforecast']['forecastday'][0]
-	loc = obs["display_location"]
 	# build wind
 	if obs['wind_kph'] == 0:
 		wind = "Calm wind."
@@ -61,11 +67,11 @@ def weather(event, bot):
 			obs['wind_gust_mph'], obs['wind_gust_kph'])
 	
 	if obs['windchill_string'] == "NA":
-		return bot.say(WEATHER_RPL % (loc['city'], loc['state'], loc['country_iso3166'], obs['temp_f'], obs['temp_c'],
+		return bot.say(WEATHER_RPL % (_build_locname(obs["display_location"]), obs['temp_f'], obs['temp_c'],
 			fore['low']['fahrenheit'], fore['high']['fahrenheit'], fore['low']['celsius'], fore['high']['celsius'], 
 			obs['weather'], obs['relative_humidity'], wind, outlook))
 	else:
-		return bot.say(WEATHER_RPL_WC % (loc['city'], loc['state'], loc['country_iso3166'], obs['temp_f'], obs['temp_c'], obs['windchill_f'], obs['windchill_c'],
+		return bot.say(WEATHER_RPL_WC % (_build_locname(obs["display_location"]), obs['temp_f'], obs['temp_c'], obs['windchill_f'], obs['windchill_c'],
 			fore['low']['fahrenheit'], fore['high']['fahrenheit'], fore['low']['celsius'], fore['high']['celsius'],
 			obs['weather'], obs['relative_humidity'], wind, outlook))
 	
@@ -92,7 +98,7 @@ def forecast(event, bot):
 		
 		today = False
 	
-	return bot.say(FORECAST_RPL % (loc['city'], loc['state'], loc['country_iso3166'], ", ".join(days)))
+	return bot.say(FORECAST_RPL % (_build_locname(loc), ", ".join(days)))
 
 def init(bot):
 	global WUAPI_MODULE # oh nooooooooooooooooo

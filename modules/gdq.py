50c96f0cb5feb8180cee91d406d37db55504419e
Clam <clam@nyanya.org>
2015-01-06 18:18:20 +1100

    Add remaining time of current to gdq

--- modules/gdq.py
+++ modules/gdq.py
@@ -4,6 +4,8 @@ from util import Mapping
 from urllib2 import build_opener
 from lxml.html import parse
 from json import load
+from time import gmtime, strptime
+from calendar import timegm # silly python... I just want UTC seconds
 
 GDQ_URL = "https://gamesdonequick.com/schedule"
 TWITCH_API_URL = "https://api.twitch.tv/kraken/channels/gamesdonequick"
@@ -17,7 +19,9 @@ def agdq(event, bot):
 	game = "Don't know"
 	eta = "?"
 	if f.getcode() == 200:
-		game = load(f)['game']
+		data = load(f)
+		game = data['game']
+		gstart = timegm(strptime(data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")) #"2015-01-06T01:11:32Z" UTC
 		ngame = game.lower()
 		o = build_opener()
 		o.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')]
@@ -35,7 +39,7 @@ def agdq(event, bot):
 				upcoming.append("\x02%s\x02 by %s (%s)" % (gdata[1], gdata[2], gdata[4].lstrip("0:")[:-3]))
 			elif gdata[1].lower() == ngame:
 				found = True
-				eta = gdata[4].lstrip("0:")[:-3]
+				eta = gdata[4]
 		# try searching for incorrect name in timetable:
 		if not found:
 			ngame = ngame.replace(":", "")
@@ -44,7 +48,12 @@ def agdq(event, bot):
 					upcoming.append("\x02%s\x02 by %s (%s)" % (gdata[1], gdata[2], gdata[4].lstrip("0:")[:-3]))
 				elif gdata[1].lower() == ngame:
 					found = True
-					eta = gdata[4].lstrip("0:")[:-3]
+					eta = gdata[4]
+		if eta:
+			curr = timegm(gmtime())
+			neta = timegm(strptime(eta, "%H:%M:%S")) - timegm(strptime("0:00:00", "%H:%M:%S"))
+			print neta, curr, gstart, curr-gstart
+			eta = "%s/%s" % (eta.lstrip("0:")[:-3], (neta - (curr-gstart))/60)
 	if not upcoming: upcoming = ["Don't know"]
 	bot.say(RPL % (game, eta, "http://www.twitch.tv/gamesdonequick/popout", "https://gamesdonequick.com/schedule"), 
 		strins=", ".join(upcoming))

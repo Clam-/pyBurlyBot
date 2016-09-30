8cf22fe08e52464d024f47d8c6f7bd9a7a7a0b8c
griffin <grause@gmail.com>
2016-09-30 17:09:16 -0400

    Random module enhancements, closes #75

--- modules/pbm_random.py
+++ modules/pbm_random.py
@@ -1,32 +1,52 @@
 # random module
-from random import choice, randint
+from random import choice, randint, random
 
 from util import Mapping, functionHelp, argumentSplit
 
 COIN = ("heads", "tails")
 
-def doflip(event, bot):
-	""" coinflip. choice flip will return randomly "heads" or "tails"."""
-	bot.say("%s" % choice(COIN))
 
-def dochoice(event, bot):
-	""" choice values. choice will randomly select one of the given values."""
-	if not event.argument: return bot.say(functionHelp(dochoice))
+def do_flip(event, bot):
+	""" coinflip. choice flip will return randomly "heads" or "tails"."""
+	if random() > 0.999:
+		bot.say('The coin landed on its side.')
+	else:
+		bot.say("%s" % choice(COIN))
+
+
+def do_choice(event, bot):
+	""" choice [value...]. choice will randomly select one of the given values,
+	or if only one value a random character from the given value."""
+	if not event.argument: return bot.say(functionHelp(do_choice))
 	values = argumentSplit(event.argument, -1)
+	if len(values) == 1:
+		values = values[0]
+	elif len(set(values)) == 1:
+		# Only duplicate values
+		return bot.say("%s, obviously." % values[0])
 	return bot.say("%s" % choice(values))
 
-def dorand(event, bot):
+
+def do_rand(event, bot):
 	""" rand [arg]. If no arg rand will generate random int between 0-10. If arg is an integer
 	value a random int between 0-arg will be generated. arg can also be a list of items, in which case will act like choice."""
 	if not event.argument:
 		return bot.say("%s" % randint(0, 10))
-	try:
-		return bot.say("%s" % randint(0, int(event.argument)))
-	except ValueError:
-		dochoice(event, bot)
-	
 
-mappings = (Mapping(command=("rand", "random"), function=dorand), 
-	Mapping(command="choice", function=dochoice),
-	Mapping(command=("coinflip", "heads", "tails", "flip"), function=doflip),)
+	try:
+		num = int(event.argument)
+	except ValueError:
+		do_choice(event, bot)
+	else:
+		if num < 0:
+			return bot.say("A number between %d and 0: %d" % (num, randint(num, 0)))
+		elif num == 0:
+			return bot.say("Zero.")
+		else:
+			return bot.say("A number between 0 and %d: %d" % (num, randint(0, num)))
+
+
+mappings = (Mapping(command=("rand", "random"), function=do_rand),
+			Mapping(command="choice", function=do_choice),
+			Mapping(command=("coinflip", "heads", "tails", "flip"), function=do_flip),)
 

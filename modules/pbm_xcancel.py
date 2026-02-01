c32d81f8257be510a2ea02d449def6a300f911ee
Clam- <985835+Clam-@users.noreply.github.com>
2026-02-01 11:27:56 +1100

    Update pbm_xcancel.py
    ...lol pls.

--- modules/pbm_xcancel.py
+++ modules/pbm_xcancel.py
@@ -1,4 +1,4 @@
-from re import compile as recompile
+from re import compile as recompile, IGNORECASE, UNICODE
 from util import Mapping
 
 def remove_elon(event, bot):
@@ -6,4 +6,4 @@ def remove_elon(event, bot):
 	posturlportion = match.group(1)
 	bot.say("https://xcancel.com/{0}".format(posturlportion))
 
-mappings = (Mapping(types=["privmsged"], regex=recompile(r"https?://(?:x\.com|twitter\.com)/(\S+)", re.IGNORECASE|UNICODE), function=remove_elon),)
+mappings = (Mapping(types=["privmsged"], regex=recompile(r"https?://(?:x\.com|twitter\.com)/(\S+)", IGNORECASE|UNICODE), function=remove_elon),)

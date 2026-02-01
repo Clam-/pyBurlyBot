261670beb02e74ade0a08783e98b4fa78f261f1a
Clam- <985835+Clam-@users.noreply.github.com>
2026-02-01 11:23:54 +1100

    Update pbm_xcancel.py
    Fix tab mistake. Update regex to be more generic.

--- modules/pbm_xcancel.py
+++ modules/pbm_xcancel.py
@@ -1,6 +1,6 @@
 def remove_elon(event, bot):
 	match = event.regex_match
 	posturlportion = match.group(1)
-  bot.say("https://xcancel.com/{0}".format(posturlportion))
+	bot.say("https://xcancel.com/{0}".format(posturlportion))
 
-mappings = (Mapping(types=["privmsged"], regex=recompile( r"https?://(?:x\.com|twitter\.com)/([^/]+/status/\d+)", re.IGNORECASE|UNICODE) function=remove_elon),)
+mappings = (Mapping(types=["privmsged"], regex=recompile(r"https?://(?:x\.com|twitter\.com)/(\S+)"", re.IGNORECASE|UNICODE) function=remove_elon),)
